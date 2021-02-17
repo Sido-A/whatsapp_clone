613ffdb63c7391150b9956731a7d0a041e3f848a
Sido-A <sidonie.abe.uk@gmail.com>
2021-02-17 11:59:55 +0000

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC0-rYXv2njVnue9q_GGjHc8kwSf7hJd2s",
+  authDomain: "whatsapp-clone-21505.firebaseapp.com",
+  projectId: "whatsapp-clone-21505",
+  storageBucket: "whatsapp-clone-21505.appspot.com",
+  messagingSenderId: "227956969108",
+  appId: "1:227956969108:web:7139b58ccbc1f378072e81",
+};
